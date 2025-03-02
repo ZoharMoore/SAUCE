8681abc6684c73c77d5d354b623b46a75b4bf6dc
ZoharMoore <zoharmoore@gmail.com>
2025-03-03 01:39:07 +0200

    gpt now runnable

--- persons/batch/__init__.py
+++ persons/batch/__init__.py
@@ -3,10 +3,12 @@ from typing import Type
 from functools import cache
 from .batch_person import BatchedPerson
 from .batch_hugging_face import PersonHuggingFace
+from .batch_gpt_4 import BatchedPerson4_0
 
 
 @cache
 def get_batch_dict() -> dict[str, Type[BatchedPerson]]:
     return {
-        "batch" + PersonHuggingFace.PERSON_TYPE: PersonHuggingFace
+        "batch" + PersonHuggingFace.PERSON_TYPE: PersonHuggingFace,
+        "batch" + BatchedPerson4_0.PERSON_TYPE: BatchedPerson4_0,  # Add mapping for BatchedPerson4_0
     }
